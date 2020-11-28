98d6756aa4d62c763101a16a03d19ac8c41a4bf2
aamnahramzan <65729262+aamnahramzan@users.noreply.github.com>
2020-11-28 14:45:22 +0000

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Subject: $subject \n Message: $message";
+$recipient = "aamnahramzan40@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You For Getting In Touch!";
+?>
